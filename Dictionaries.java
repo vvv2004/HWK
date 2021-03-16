666a8ad630ff83428612dc463ff3d648256ef7b0
vvv2004 <viktorvalentinov14@abv.bg>
2021-03-16 20:34:59 +0200

    Create Dictionaries.java

---
+++ Dictionaries.java
@@ -0,0 +1,29 @@
+package Krasi.Homework.HWK;
+
+import java.util.HashMap;
+
+public class Dictionaries {
+    public static void main(String[] args) {
+        HashMap<Integer, String> months = new HashMap<Integer, String>();
+
+        defineMonths(months);
+
+        System.out.println(months);
+    }
+
+    private static void defineMonths(HashMap<Integer, String> months) {
+        months.put(1, "January");
+        months.put(2, "February");
+        months.put(3, "March");
+        months.put(4, "April");
+        months.put(5, "May");
+        months.put(6, "June");
+        months.put(7, "July");
+        months.put(8, "August");
+        months.put(9, "September");
+        months.put(10, "October");
+        months.put(11, "November");
+        months.put(12, "December");
+    }
+
+}
