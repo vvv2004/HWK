a72ed6ad4727ce222a35479e311c3cc794c2f3b0
vvv2004 <viktorvalentinov14@abv.bg>
2021-03-16 20:45:15 +0200

    Included commands
    Included commands to the program, the functions executing the commands are still work in progress

--- Dictionaries.java
+++ Dictionaries.java
@@ -1,17 +1,28 @@
 package Krasi.Homework.HWK;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Dictionaries {
     public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+
         HashMap<Integer, String> months = new HashMap<Integer, String>();
+        fillMonths(months);
 
-        defineMonths(months);
+        System.out.print("Input command: ");
+        String command = read.nextLine();
 
-        System.out.println(months);
+        while(!command.equals("stop")){
+            if(command.equals("num")){
+                //search by number
+            }else if(command.equals("name")){
+                //search by name
+            }
+        }
     }
 
-    private static void defineMonths(HashMap<Integer, String> months) {
+    private static void fillMonths(HashMap<Integer, String> months) {
         months.put(1, "January");
         months.put(2, "February");
         months.put(3, "March");
